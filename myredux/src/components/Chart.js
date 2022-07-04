0f3218ff391b7e2f84cdc71bc50e40b277ea62e6
Purnima-1 <baranwal.purnimagmail.com>
2022-07-04 11:25:48 -0700

    working on chart

--- myredux/src/components/Chart.js
+++ myredux/src/components/Chart.js
@@ -1,11 +1,25 @@
 import React from 'react'
-import {Doughnut} from 'react-chartjs-2'
+import { Doughnut } from 'react-chartjs-2'
+import {CategoryScale} from 'chart.js'; 
+import Chart from 'chart.js/auto';
+Chart.register(CategoryScale);
 
-const Chart = () => {
+const Charts = () => {
+  //const data = useSelector((state)=>state.actionR.value )
+  const data = {
+    labels: ['Red','Blue','Yellow','Green','Orange'],
+    datasets: [{
+      label:'Scoring Volleyball',
+      data: [3,15,4,19,8],
+      backgroundColor: ['pink','skyBlue','lightyellow','lightgreen','orange']
+    }]
+  }
   return (
-    <h1>Doughnut Chart</h1>
-
+    <div style={{maxWidth:'650px',margin:'0 auto'}}>
+    <h1>Line Chart</h1>
+    <Doughnut data = {data}/>
+    </div>
   )
 }
 
-export default Chart
+export default Charts
--- myredux/src/reducers/actionReducer.js
+++ myredux/src/reducers/actionReducer.js
@@ -5,10 +5,12 @@ name: "action",
 //initialState: {value:teamData},
 //initialState: {value: []},
 //initialState: localStorage.getItem("value") ? JSON.parse(localStorage.getItem("value")) : [{id:null,team:'',score:'',action:''}],
-initialState: {value: [{id:null,team:'',score:'',action:''}]},
+initialState: {value: localStorage.getItem('value')?JSON.parse(localStorage.getItem('value'))
+    :[{id:null,team:'',score:'',action:''}]},
 reducers: {
  addTeam: (state,action) => {
      state.value.push(action.payload)
+     localStorage.setItem('value',JSON.stringify(state.value))
  },
  deleteTeam: (state,action)=> {
      state.value = state.value.filter((t)=>t.id !== action.payload.id )
